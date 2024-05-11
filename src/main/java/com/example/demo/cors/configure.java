501a675e1d4afa73dac1879c4b970dbfa630ca61
Bruno Gressler da Silveira <brunogressler1@gmail.com>
2024-05-11 16:47:27 -0300

    config cors

---
+++ src/main/java/com/example/demo/cors/configure.java
@@ -0,0 +1,25 @@
+package com.example.demo.cors;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class configure implements WebMvcConfigurer {
+
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer() {
+        System.out.println("INTO webMvcConfigurer");
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry
+                        .addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedHeaders("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+            }
+        };
+    }
+}
